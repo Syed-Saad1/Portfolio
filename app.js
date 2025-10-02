b94081de3488d5c305157910196bc7ef5bbe6d37
syed-saad-1 <sfurqanarif7528@gmail.com>
2025-10-01 17:54:15 -0700

    first  commit1

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Connected");
+
+// Toggle Menu
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+});
