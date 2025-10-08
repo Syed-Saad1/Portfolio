96bbd9d6eaadfe86e627ece3f53fddb602756af8
syed-saad-1 <sfurqanarif7528@gmail.com>
2025-10-07 17:35:46 -0700

    Final Portfolio

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-console.log("Connected");
+ const menuBtn = document.getElementById('menuBtn');
+  const menuIcon = document.getElementById('menuIcon');
+  const mobileMenu = document.getElementById('mobileMenu');
 
-// Toggle Menu
-const menuBtn = document.getElementById("menu-btn");
-const mobileMenu = document.getElementById("mobile-menu");
+  let isOpen = false;
 
-menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-});
+  menuBtn.addEventListener('click', () => {
+    isOpen = !isOpen;
+
+    if (isOpen) {
+      mobileMenu.classList.remove('hidden');
+      setTimeout(() => {
+        mobileMenu.classList.remove('scale-y-0');
+        mobileMenu.classList.add('scale-y-100');
+      }, 10);
+      menuIcon.src = './Accets/close (2).png'; // replace with your "close" icon
+    } else {
+      mobileMenu.classList.remove('scale-y-100');
+      mobileMenu.classList.add('scale-y-0');
+      setTimeout(() => {
+        mobileMenu.classList.add('hidden');
+      }, 300);
+      menuIcon.src = './Accets/nav menu (1).png';
+    }
+  });
